0ecd37b9284178caac7b0253e9628512560dddbd
ikjot-2605 <ikjotsd@gmail.com>
2020-06-03 23:58:48 +0530

    added necessary keys to connect to firebase

---
+++ app.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD-v_aiEkCcVzHAJbx0EPRRWDNchzObNZo",
+    authDomain: "help-india-june-2020.firebaseapp.com",
+    databaseURL: "https://help-india-june-2020.firebaseio.com",
+    projectId: "help-india-june-2020",
+    storageBucket: "help-india-june-2020.appspot.com",
+    messagingSenderId: "29672385815",
+    appId: "1:29672385815:web:c577a3015c6b7d69d79b52",
+    measurementId: "G-L91D6EFNX5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
